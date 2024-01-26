ec8745e757b66d82af5fad9179624df7d4d87eaa
KD Ninama <krunal16062000@gmail.com>
2024-01-27 02:01:38 +0530

    add 404 page

--- ThreadWoven/ThreadWovenApp/views.py
+++ ThreadWoven/ThreadWovenApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def custom_404_view(request, exception):
+    return render(request, '404.html')
